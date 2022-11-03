afcc768ce1dc14ea731ea41ff922cf1e91ba27f8
Kou Yamamoto <ykk.7234@gmail.com>
2022-11-03 11:52:18 +0900

    feat: `ViewBinding`を利用してUIイベントを検知

--- app/src/main/java/com/example/androidsample/MainActivity.kt
+++ app/src/main/java/com/example/androidsample/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.androidsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.example.androidsample.databinding.ActivityMainBinding
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonCurrent.setOnClickListener {
+            binding.txtResult.text = Date().toString()
+        }
     }
 }
