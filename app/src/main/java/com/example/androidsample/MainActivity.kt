9c60a870d52f18ffd73f5397d6d0576d94cef7a0
Kou Yamamoto <ykk.7234@gmail.com>
2022-11-04 13:43:09 +0900

    feat: `spinner`のイベントを検知

--- app/src/main/java/com/example/androidsample/MainActivity.kt
+++ app/src/main/java/com/example/androidsample/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.androidsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.RadioButton
+import android.widget.Spinner
+import android.widget.Toast
+import com.example.androidsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "選択項目${(parent as Spinner).selectedItem}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "何も選択されませんでした",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     }
 }
