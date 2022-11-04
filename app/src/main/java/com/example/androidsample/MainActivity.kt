27e3bfd736afe9e77aa9ca393ee272d6090f64dc
Kou Yamamoto <ykk.7234@gmail.com>
2022-11-04 14:16:07 +0900

    feat: `ratingBar`のイベントを検知するようにした

--- app/src/main/java/com/example/androidsample/MainActivity.kt
+++ app/src/main/java/com/example/androidsample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.androidsample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.androidsample.databinding.ActivityMainBinding
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +13,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.ratingBar.setOnRatingBarChangeListener { _, rate, _ ->
+            Toast.makeText(
+                this@MainActivity,
+                "現在の評価は${rate}です",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
