ed8a8ae782ce6a29f95b353e96ad2f1edf69895e
Kou Yamamoto <ykk.7234@gmail.com>
2022-11-04 14:07:11 +0900

    feat: `spinner`のリストを動的に変更

--- app/src/main/java/com/example/androidsample/MainActivity.kt
+++ app/src/main/java/com/example/androidsample/MainActivity.kt
@@ -5,10 +5,13 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Adapter
 import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.RadioButton
 import android.widget.Spinner
 import android.widget.Toast
 import com.example.androidsample.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +22,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        createSpinner()
+    }
+
+    private fun createSpinner() {
+        val list = mutableListOf<String>()
+        val formatter = SimpleDateFormat("yyyy/MM/dd", Locale.JAPAN)
+        val calendar = Calendar.getInstance()
+
+        for(i in 1..10) {
+            calendar.set(Calendar.DAY_OF_MONTH, calendar.get(Calendar.DAY_OF_MONTH) + 1)
+            list.add(formatter.format(calendar.time))
+        }
+
+        binding.spinner.adapter = ArrayAdapter(
+            this,
+            androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+            list
+        )
 
         binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
