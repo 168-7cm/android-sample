658374da49a8ec6f76afadd0a4c959692fed83fe
Kou Yamamoto <ykk.7234@gmail.com>
2022-11-13 23:05:18 +0900

    feat: 専用の選択画面を持った`ListView`を実装

--- app/src/main/java/com/example/androidsample/MainActivity.kt
+++ app/src/main/java/com/example/androidsample/MainActivity.kt
@@ -1,11 +1,64 @@
 package com.example.androidsample
 
+import android.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ActionMode
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.AbsListView
+import android.widget.ArrayAdapter
+import android.widget.CheckedTextView
+import android.widget.Toast
+import androidx.core.view.forEach
+import com.example.androidsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val data = mutableListOf("胡椒", "コリアンダー", "生姜", "ニンニク")
+        val adapter = ArrayAdapter(
+            this,
+            R.layout.simple_list_item_checked,
+            data
+        )
+
+        val listView = binding.listView
+        listView.adapter = adapter
+
+        listView.setMultiChoiceModeListener(object: AbsListView.MultiChoiceModeListener {
+            override fun onCreateActionMode(p0: ActionMode?, p1: Menu?): Boolean {
+                return true
+            }
+
+            override fun onPrepareActionMode(p0: ActionMode?, p1: Menu?): Boolean {
+                return true
+            }
+
+            override fun onItemCheckedStateChanged(p0: ActionMode?, p1: Int, p2: Long, p3: Boolean) {}
+
+            override fun onActionItemClicked(p0: ActionMode?, p1: MenuItem?): Boolean {
+                return true
+            }
+
+            override fun onDestroyActionMode(p0: ActionMode?) {
+                val msg = mutableListOf<CharSequence>()
+                listView.forEach {
+                    val check = it as CheckedTextView
+                    if (check.isChecked) msg.add(check.text)
+                }
+
+                Toast.makeText(
+                    this@MainActivity,
+                    "選択したのは${msg.joinToString()}です",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
     }
 }
