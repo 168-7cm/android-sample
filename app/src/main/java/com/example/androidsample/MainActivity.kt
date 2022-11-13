526579e880b6e47e91b426dec9082454976cd8d0
Kou Yamamoto <ykk.7234@gmail.com>
2022-11-13 22:56:44 +0900

    feat: 複数選択可能な`ListView`を実装

--- app/src/main/java/com/example/androidsample/MainActivity.kt
+++ app/src/main/java/com/example/androidsample/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.androidsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.CheckedTextView
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.forEach
+import com.example.androidsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val data = mutableListOf("胡椒", "コリアンダー", "生姜", "ニンニク")
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_checked,
+            data
+        )
+
+        val listView = binding.listView
+        listView.adapter = adapter
+
+        listView.setOnItemClickListener { av, view, position, id ->
+            val msg = mutableListOf<CharSequence>()
+
+            listView.forEach {
+                val check = it as CheckedTextView
+                if (check.isChecked) msg.add(check.text)
+            }
+
+            Toast.makeText(
+                this@MainActivity,
+                "選択したのは${msg.joinToString()}です",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
